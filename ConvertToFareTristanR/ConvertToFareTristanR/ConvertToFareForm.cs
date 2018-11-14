07b0d64a9523a04ca6b4e4aaed869eaae64684ee
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-14 10:41:41 -0500

    Initial commit

---
+++ ConvertToFareTristanR/ConvertToFareTristanR/ConvertToFareForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertToFareTristanR
+{
+	public partial class ConvertToFareForm : Form
+	{
+		public ConvertToFareForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
