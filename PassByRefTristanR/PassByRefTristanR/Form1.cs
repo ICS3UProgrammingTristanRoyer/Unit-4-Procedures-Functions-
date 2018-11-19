bb87df1505534258b29068b1b4186e9c27049bba
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-11-19 11:06:09 -0500

    ok

--- PassByRefTristanR/PassByRefTristanR/Form1.cs
+++ PassByRefTristanR/PassByRefTristanR/Form1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Tristan Roye
+ * Created on: 19 - 11 - 18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #31 - Rounding Program
+ * This program takes the decimal given by the user and rounds it to the number of places the user wants.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,17 @@ namespace PassByRefTristanR
 		public Form1()
 		{
 			InitializeComponent();
+		}
+		//Procedure: RoundNumber
+		//Input: ref txtNumber , int numberOfPlaces
+		//Output: void
+		//Description: This procedure takes the decimal given by the user and rounds it to the places that the user wants.
+		private void RoundDecimal (ref Double decimalNumber , int numOfPlaces)
+		{
+			}
+		private void button1_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
