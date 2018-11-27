cae1f972027b56be2617d0412c079dfeba01c864
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-11-27 00:14:16 -0500

    okok

--- NewWalkingTristanR/NewWalkingTristanR/Form1.cs
+++ NewWalkingTristanR/NewWalkingTristanR/Form1.cs
@@ -16,5 +16,35 @@ namespace NewWalkingTristanR
 		{
 			InitializeComponent();
 		}
+
+		private void GeneratePictureBoxes()
+		{
+			// generate each of the picture boxes on the form.
+
+			GeneratePictureBoxes(61, 78);
+			GeneratePictureBoxes(464, 78);
+			GeneratePictureBoxes(61, 358);
+			GeneratePictureBoxes(464, 358);
+        }
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			// reveal the instructions
+			this.lblInstructions.Show();
+
+			// generate the picture boxes with their event listeners.
+			GeneratePictureBoxes();
+
+		}
+
+		private void GeneratePictureBox(int x , int y)
+		{
+			// dynamically generate a new picture box and a new point at the given location
+			PictureBox topPicMan = new PictureBox();
+			Point pntPic = new System.Drawing.Point(x, y);
+
+			// set the location of the picture box.
+			top
+		}
 	}
 }
